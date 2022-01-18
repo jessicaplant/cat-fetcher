f2018a3aa677e57eb839b47a8e184f3863904dd4
Jessica Plant <jessicaplant@Jessicas-Air.plantwire.co.uk>
2022-01-18 10:31:46 +0000

    adds call to get the cats

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = async function getCats() {
+    const axios = require('axios');
+    let result;
+
+    await axios
+        .get('https://cat-fact.herokuapp.com/facts')
+        .then((response) => {
+            return response
+        })
+        .then((cats) => {
+            result = cats.data.objects;
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+    return result;
+}
